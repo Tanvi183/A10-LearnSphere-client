6fd40156a9afbdb4e15e5ac03f0c508608887c93
tanvi183 <mneshat7@gmail.com>
2025-11-13 04:29:14 -0800

    Enrolled Course Data load according to user.

--- src/components/Courses/SidebarFilters.jsx
+++ src/components/Courses/SidebarFilters.jsx
@@ -17,11 +17,18 @@ const FilterSection = ({ title, options }) => (
 export default function SidebarFilters() {
   return (
     <aside className="hidden lg:block w-64 bg-white border border-gray-200 rounded-xl p-6 h-fit">
-      <FilterSection title="Categories" options={["Art & Design", "Business", "Data Science", "Development", "Finance", "Health & Fitness", "Lifestyle"]} />
-      <FilterSection title="Language" options={["English", "Arabic", "Spanish"]} />
-      <FilterSection title="Price" options={["Free", "Paid"]} />
-      <FilterSection title="Skill Level" options={["Beginner", "Intermediate", "High"]} />
-      <FilterSection title="Ratings" options={["★★★★★", "★★★★☆", "★★★☆☆"]} />
+      <FilterSection
+        title="Categories"
+        options={[
+          "Art & Design",
+          "Business",
+          "Data Science",
+          "Development",
+          "Finance",
+          "Health & Fitness",
+          "Lifestyle",
+        ]}
+      />
     </aside>
   );
 }
--- src/pages/MyEnrollments.jsx
+++ src/pages/MyEnrollments.jsx
@@ -1,59 +1,77 @@
-import React from "react";
-import { Clock, Users, Star } from "lucide-react";
+import React, { use, useEffect, useState } from "react";
 import CourseCard from "../components/Courses/CourseCard";
+import { AuthContext } from "../context/AuthContext";
 
 const MyEnrollments = () => {
-  const courses = [
-    {
-      id: 1,
-      title: "Learning JavaScript With Imagination",
-      category: "Development",
-      level: "Expert",
-      type: "Laravel Pro",
-      instructor: "David Millar",
-      rating: 4.8,
-      reviews: 22,
-      progress: 88,
-      image: "https://i.ibb.co.com/qL1J2b5d/demo-app-6.webp",
-      duration: "11h 20m",
-      lessons: 5,
-      students: 22,
-    },
-    {
-      id: 2,
-      title: "The Complete Graphic Design for Beginners",
-      category: "Design",
-      level: "Beginner",
-      type: "Crush Course",
-      instructor: "Wilson",
-      rating: 4.5,
-      reviews: 202,
-      progress: 70,
-      image: "https://i.ibb.co.com/qL1J2b5d/demo-app-6.webp",
-      duration: "70h 45m",
-      lessons: 60,
-      students: 202,
-    },
-    {
-      id: 3,
-      title: "Learning JavaScript With Imagination",
-      category: "Data Science",
-      level: "Marketing",
-      type: "Pro Expert",
-      instructor: "Warren",
-      rating: 4.8,
-      reviews: 66,
-      progress: 95,
-      image: "https://i.ibb.co.com/qL1J2b5d/demo-app-6.webp",
-      duration: "18h 20m",
-      lessons: 8,
-      students: 66,
-    },
-  ];
+  const { user } = use(AuthContext);
+  const [courses, setcourses] = useState([]);
+  console.log(courses);
+
+  // const courses = [
+  //   {
+  //     id: 1,
+  //     title: "Learning JavaScript With Imagination",
+  //     category: "Development",
+  //     level: "Expert",
+  //     type: "Laravel Pro",
+  //     instructor: "David Millar",
+  //     rating: 4.8,
+  //     reviews: 22,
+  //     progress: 88,
+  //     image: "https://i.ibb.co.com/qL1J2b5d/demo-app-6.webp",
+  //     duration: "11h 20m",
+  //     lessons: 5,
+  //     students: 22,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "The Complete Graphic Design for Beginners",
+  //     category: "Design",
+  //     level: "Beginner",
+  //     type: "Crush Course",
+  //     instructor: "Wilson",
+  //     rating: 4.5,
+  //     reviews: 202,
+  //     progress: 70,
+  //     image: "https://i.ibb.co.com/qL1J2b5d/demo-app-6.webp",
+  //     duration: "70h 45m",
+  //     lessons: 60,
+  //     students: 202,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Learning JavaScript With Imagination",
+  //     category: "Data Science",
+  //     level: "Marketing",
+  //     type: "Pro Expert",
+  //     instructor: "Warren",
+  //     rating: 4.8,
+  //     reviews: 66,
+  //     progress: 95,
+  //     image: "https://i.ibb.co.com/qL1J2b5d/demo-app-6.webp",
+  //     duration: "18h 20m",
+  //     lessons: 8,
+  //     students: 66,
+  //   },
+  // ];
+
+  useEffect(() => {
+    if (user?.email) {
+      fetch(`http://localhost:5000/enrollment?email=${user.email}`, {
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setcourses(data);
+        });
+    }
+  }, [user]);
 
   return (
     <div className="max-w-7xl mx-auto p-4 md:p-6">
-      <h2 className="text-xl font-semibold mb-6">Enrolled Courses</h2>
+      <h2 className="text-xl font-semibold mb-6">
+        Enrolled Courses : {courses.length}
+      </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {courses.map((course) => (
