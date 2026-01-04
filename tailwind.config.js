de70783f3f01cb3e0cee61cd26d7d7b56760236e
tanvi183 <mneshat7@gmail.com>
2026-01-04 19:56:02 +0600

    My enrollmentpage changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class", '[data-theme="dark"]'],
@@ -27,3 +31,4 @@ export default {
     ],
   },
 };
+
